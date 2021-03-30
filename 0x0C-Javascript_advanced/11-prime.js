4c472c09599591bae3beac0c493a712bbabf00c4
earamosb8 <earamosb8@gmail.com>
2021-03-30 18:38:57 -0500

    add task 12

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,31 @@
+/*Write a function named countPrimeNumbers:
+It will return the number of prime numbers from 2 to 100
+*/
+function countPrimeNumbers(){
+    let primes = 0;
+
+    for(let i = 2 ; i <= 100; i++) {
+        primes += 1;
+        let count = 0;
+        for(let j = 1; j <= i; j++) {
+                if(i % j == 0){
+                    count += 1;
+                    if(count === 3){
+                        primes -= 1;
+                        break;
+                    }
+                    
+                }
+         }
+    }
+    return primes;
+}
+
+let t0 = performance.now();
+setTimeout(function () {
+    for (let i = 0; i < 100; i++) {
+        countPrimeNumbers();
+    }
+}, 0)
+let t1 = performance.now();
+console.log("Execution time of calculating prime numbers 100 times was " + (t1 - t0) + " milliseconds.");
