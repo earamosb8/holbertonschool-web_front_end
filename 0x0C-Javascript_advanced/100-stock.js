c8f7c63b8d4031d8704b7e14227680a62e56c704
earamosb8 <earamosb8@gmail.com>
2021-03-30 19:39:01 -0500

    add 100-stock

---
+++ 0x0C-Javascript_advanced/100-stock.js
@@ -0,0 +1,31 @@
+/*Create a variable named stock:
+It contains the stock for the items you are selling
+It’s a dictionary with the values macbook: 2 and iphone: 4*/
+
+let stock = {
+    macbook: 2,
+    iphone: 4
+}
+
+function processPayment (itemName) {
+    stock[itemName] -= 1;
+    console.log(`Payment is being processed for item ${itemName}`);
+}
+
+function processError (itemName) {
+    console.log(`No more ${itemName} in stock`);
+    console.log('Payment is not being processed')
+    
+}
+
+function processOrder (itemName, callbackPayment, callbackError) {
+    let itemNameFormated = itemName.toLowerCase();
+    if (!stock[itemNameFormated]) {
+        return;
+    }
+
+    stock[itemNameFormated] == 0 ? callbackError(itemNameFormated) : callbackPayment(itemNameFormated)
+}
+
+let text  = prompt("Please enter the item you would like to purchase (Macbook, iPhone)");
+processOrder(text, processPayment, processError);
